d9c256f339236b46c7c9472bdceb414cb0986d07
John D'Agostino <john@ordermentum.com>
2022-03-18 12:47:05 +1100

    restore cache implementation

--- packages/asap-core/src/fetch.ts
+++ packages/asap-core/src/fetch.ts
@@ -1,6 +1,8 @@
 import assert from 'assert';
 import { URL } from 'url';
 import axios from 'axios';
+import cache from './cache';
+import { AsapAuthenticationError } from './errors';
 
 function parseUrlArray(array: string[]) {
   const urls = array.map(urlString => new URL(urlString));
@@ -29,13 +31,33 @@ export function createPublicKeyFetcher({
   );
 
   const clients = validatedPublicKeyBaseUrls.map(baseUrl =>
-    axios.create({ baseURL: baseUrl, timeout: 15000 })
+    axios.create({
+      baseURL: baseUrl,
+      headers: {
+        Accept: 'application/x-pem-file',
+      },
+      timeout: 15000,
+    })
   );
 
-  return async function getPublicKey(keyId: string) {
-    const promises = clients.map(client => client.get(keyId));
-    const response = await Promise.race(promises);
-    return response.data;
+  return async function getPublicKey(
+    keyId: string
+  ): Promise<string | undefined> {
+    let value = cache.get<string>(keyId);
+
+    if (value) {
+      return value;
+    }
+
+    const promises = clients.map(client => client.get<string>(keyId));
+    const response = await Promise.any(promises);
+    if (response.status === 200) {
+      value = response.data;
+      cache.set(keyId, value);
+      return value;
+    }
+
+    throw new AsapAuthenticationError('failed to fetch public key');
   };
 }
 
