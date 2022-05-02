061a032418483fe30d9f3bb41cec8e1ed8d2c371
John D'Agostino <john@ordermentum.com>
2022-05-02 17:16:28 +1000

    Add support to load private keys from file (#15)

---
+++ packages/asap-core/test/file_test.ts
@@ -0,0 +1,37 @@
+import { describe, afterEach, beforeEach } from 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { createPublicKeyFetcher } from '../src/fetchers/file';
+
+describe('file', () => {
+  let jwtConfig;
+  let currentTime;
+  let time;
+
+  beforeEach(() => {
+    time = sinon.useFakeTimers(123456789 * 1000);
+  });
+
+  afterEach(() => {
+    time.restore();
+  });
+
+  describe('fileLoader', () => {
+    it('correctly loads file value', async () => {
+      const fetcher = createPublicKeyFetcher(`${__dirname}/data/`);
+      const key = await fetcher('test/private');
+      expect(key).to.exist;
+    });
+
+    it('throws error', async () => {
+      const fetcher = createPublicKeyFetcher(`${__dirname}/data/`);
+      let e;
+      try {
+        const key = await fetcher('test/missing');
+      } catch (err) {
+        e = err;
+      }
+      expect(e).to.exist;
+    });
+  });
+});
