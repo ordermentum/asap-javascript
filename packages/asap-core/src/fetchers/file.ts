a994ac2784a853b7b69cd28c93f864505dbc5341
John D'Agostino <john@ordermentum.com>
2022-05-02 12:49:44 +1000

    add file loader support

---
+++ packages/asap-core/src/fetchers/file.ts
@@ -0,0 +1,17 @@
+import { promises as fs, existsSync } from 'fs';
+import path from 'path';
+
+export const createPublicKeyFetcher =
+  (directory: string) =>
+  async (keyId: string): Promise<string> => {
+    const full = path.join(directory, `${keyId}.pem`);
+
+    if (!existsSync(full)) {
+      throw new Error(`Invalid Key ${keyId} not found in ${full}`);
+    }
+
+    const file = await fs.readFile(full);
+    return file.toString('utf-8');
+  };
+
+export default createPublicKeyFetcher;
