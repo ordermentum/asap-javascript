3c2c177dbdc11f40c86c820671ef7e445f58bb2d
Ish <arshusingh16@gmail.com>
2022-06-17 16:05:32 +1000

    Set asap claims on the request object - express
    The asap claims were being set on the response object.
    It should be on the request object instead.

--- packages/express-asap/src/index.ts
+++ packages/express-asap/src/index.ts
@@ -1,2 +1,14 @@
+import { JwtPayload } from 'jsonwebtoken';
 export * from './middleware';
 export * from './whitelist_middleware';
+
+declare global {
+    namespace Express {
+        interface Request {
+            locals?: {
+                [key: string]: any;
+                asapClaims?: string | JwtPayload | null
+            }
+        }
+    }
+}
