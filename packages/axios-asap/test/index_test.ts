4ef426e2fb7b77a79d1ee1e0722ab382c1b9b684
John D'Agostino <john@ordermentum.com>
2023-04-14 13:04:31 +1000

    We currently create a client in each service.
    This moves the common code back into the axios-asap package.
    
    This offers the same options
    
    - keep alive enabled on clients
    - client caching

--- packages/axios-asap/test/index_test.ts
+++ packages/axios-asap/test/index_test.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import moxios from 'moxios';
 import { expect } from 'chai';
 import { privateKeyPem } from '@ordermentum/asap-test-helpers';
-import { createAsapInterceptor } from '../src';
+import { createAsapInterceptor, createClient } from '../src';
 
 describe('createAuthHeaderGenerator', () => {
   let jwtConfig;
@@ -28,7 +28,36 @@ describe('createAuthHeaderGenerator', () => {
 
     const res = await client.get('/say/hello');
     expect(res.status).to.equal(200);
-    expect(Object.keys(res.config.headers)).to.include('Authorization');
-    expect(res.config.headers.Authorization).to.include('Bearer');
+    expect(Object.keys(res?.config?.headers ?? {})).to.include('Authorization');
+    expect(res?.config?.headers?.Authorization).to.include('Bearer');
+  });
+
+  it('creates a client', async () => {
+    const client = createClient(
+      {
+        service: 'the-keyid',
+        issuer: 'an-issuer',
+        publicKey: 'public-key',
+        privateKey: privateKeyPem,
+      },
+      {
+        insecureMode: false,
+        additionalClaims: {
+          admin: true,
+          userId: '123',
+        },
+      }
+    );
+
+    moxios.install(client);
+    moxios.stubRequest('/say/hello', {
+      status: 200,
+      responseText: 'hello',
+    });
+
+    const res = await client.get('/say/hello');
+    expect(res.status).to.equal(200);
+    expect(Object.keys(res?.config?.headers ?? {})).to.include('Authorization');
+    expect(res?.config?.headers?.Authorization).to.include('Bearer');
   });
 });
